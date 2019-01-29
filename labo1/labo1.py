034d299bdaa4bcb211446748ea1bc3d3febc82d7
jhyacinthe <hyacinthejefferson@live.ca>
2019-01-28 23:51:53 -0500

    ajout des autres coportement

--- labo1/labo1.py
+++ labo1/labo1.py
@@ -2,6 +2,7 @@ import sys
 import time
 
 import cozmo.robot
+import asyncio
 from cozmo.objects import CustomObjectMarkers,FixedCustomObject,EvtObjectLocated,CustomObjectTypes, CustomObject,\
     EvtObjectAppeared,EvtObjectDisappeared
 from cozmo.world import World,CameraImage,EvtNewCameraImage
@@ -22,6 +23,35 @@ def handle_object_disappeared(evt, **kw):
     # whenever an Object goes out of view.
     if isinstance(evt.obj, CustomObject):
         print("Cozmo stopped seeing a %s" % str(evt.obj.object_type))
+
+
+def follow_faces(robot: cozmo.robot.Robot):
+    '''The core of the follow_faces program'''
+
+    # Move lift down and tilt the head up
+    robot.move_lift(-3)
+    robot.set_head_angle(cozmo.robot.MAX_HEAD_ANGLE).wait_for_completed()
+
+    face_to_follow = None
+
+    while True:
+        turn_action = None
+        if face_to_follow:
+            turn_action = robot.turn_towards_face(face_to_follow)
+
+        if not (face_to_follow and face_to_follow.is_visible):
+            # find a visible face, timeout if nothing found after a short while
+            try:
+                face_to_follow = robot.world.wait_for_observed_face(timeout=30)
+            except asyncio.TimeoutError:
+                print("Didn't find a face - exiting!")
+                return
+
+        if turn_action:
+            # Complete the turn action if one was in progress
+            turn_action.wait_for_completed()
+
+        time.sleep(.1)
 
 
 def take_picture(robot: cozmo.robot.Robot, i):
@@ -52,7 +82,8 @@ def take_picture(robot: cozmo.robot.Robot, i):
 #    markers_identification = robot.world.define_custom_wall(CustomObject.)
 
 def start_music(robot: cozmo.robot.Robot):
-    MusicCube = cozmo.audio.AudioEvents(name='MusicCubeWhack', id=3324716189)
+    robot.play_audio(cozmo.audio.AudioEvents.Sfx_Egg_Decorating_Crack)
+    time.sleep(2.0)
 
 
 # Desempile 2 cubes
@@ -124,7 +155,7 @@ def empileCube(robot : cozmo.robot.Robot):
         print("Cozmo successfully stacked 2 blocks!")
 
 
-def cozmo_program(robot: cozmo.robot.Robot):
+def custom_objects(robot: cozmo.robot.Robot):
 
     circles2 = CustomObjectMarkers.Circles2
     circles3 = CustomObjectMarkers.Circles3
@@ -136,17 +167,69 @@ def cozmo_program(robot: cozmo.robot.Robot):
     hexagone2 = CustomObjectMarkers.Hexagons2
     hexagone3 = CustomObjectMarkers.Hexagons3
 
+    # Add event handlers for whenever Cozmo sees a new object
+    robot.add_event_handler(cozmo.objects.EvtObjectAppeared, handle_object_appeared)
+    robot.add_event_handler(cozmo.objects.EvtObjectDisappeared, handle_object_disappeared)
+
+    # define a unique cube (44mm x 44mm x 44mm) (approximately the same size as a light cube)
+    # with a 30mm x 30mm Diamonds2 image on every face
+    cube_obj = robot.world.define_custom_cube(CustomObjectTypes.CustomType00,
+                                              CustomObjectMarkers.Diamonds2,
+                                              44,
+                                              30, 30, True)
+
+    # define a unique cube (88mm x 88mm x 88mm) (approximately 2x the size of a light cube)
+    # with a 50mm x 50mm Diamonds3 image on every face
+    big_cube_obj = robot.world.define_custom_cube(CustomObjectTypes.CustomType01,
+                                              CustomObjectMarkers.Diamonds3,
+                                              88,
+                                              50, 50, True)
+
+    # define a unique wall (150mm x 120mm (x10mm thick for all walls)
+    # with a 50mm x 30mm Circles2 image on front and back
+    wall_obj = robot.world.define_custom_wall(CustomObjectTypes.CustomType02,
+                                              CustomObjectMarkers.Circles2,
+                                              150, 120,
+                                              50, 30, True)
+
+    # define a unique box (60mm deep x 140mm width x100mm tall)
+    # with a different 30mm x 50mm image on each of the 6 faces
+    box_obj = robot.world.define_custom_box(CustomObjectTypes.CustomType03,
+                                            CustomObjectMarkers.Hexagons2,  # front
+                                            CustomObjectMarkers.Circles3,   # back
+                                            CustomObjectMarkers.Circles4,   # top
+                                            CustomObjectMarkers.Circles5,   # bottom
+                                            CustomObjectMarkers.Triangles2, # left
+                                            CustomObjectMarkers.Triangles3, # right
+                                            60, 140, 100,
+                                            30, 50, True)
+
+    if ((cube_obj is not None) and (big_cube_obj is not None) and
+            (wall_obj is not None) and (box_obj is not None)):
+        print("All objects defined successfully!")
+    else:
+        print("One or more object definitions failed!")
+        return
+
+    print("Show the above markers to Cozmo and you will see the related objects "
+          "annotated in Cozmo's view window, you will also see print messages "
+          "everytime a custom object enters or exits Cozmo's view.")
+
+    print("Press CTRL-C to quit")
+    while True:
+        time.sleep(0.1)
+
+
+def cozmo_program(robot: cozmo.robot.Robot):
+
     # Arret 1
-    markers_identification = robot.world.define_custom_wall(CustomObjectTypes.CustomType00,circles2 ,120, 100,50, 30, True)
     take_picture(robot, 1)
-    print(markers_identification)
     robot.drive_straight(distance_mm(90), speed_mmps(50)).wait_for_completed()
     print('1')
     # Arret 2 (parcours losange)
     robot.say_text(" La premiere action est de dire un texte").wait_for_completed()
     take_picture(robot, 2)
     robot.drive_straight(distance_mm(90), speed_mmps(50)).wait_for_completed()
-    start_music(robot)
     print('2')
     # Arret 3
     take_picture(robot, 3)
@@ -154,10 +237,12 @@ def cozmo_program(robot: cozmo.robot.Robot):
     print('3')
     # Arret 4 (parcours losange)
     take_picture(robot, 4)
+    start_music(robot)
     robot.drive_straight(distance_mm(90), speed_mmps(50)).wait_for_completed()
     robot.turn_in_place(degrees(90)).wait_for_completed()
     print('4')
     # Arret 5
+    custom_objects(robot)
     robot.drive_straight(distance_mm(90), speed_mmps(50)).wait_for_completed()
     robot.turn_in_place(degrees(90)).wait_for_completed()
     take_picture(robot, 5)
@@ -176,6 +261,7 @@ def cozmo_program(robot: cozmo.robot.Robot):
     take_picture(robot, 8)
     print('8')
     # Arret 9 (parcours losange)
+    follow_faces(robot)
     robot.drive_straight(distance_mm(90), speed_mmps(50)).wait_for_completed()
     robot.turn_in_place(degrees(90)).wait_for_completed()
     take_picture(robot, 9)
@@ -186,6 +272,7 @@ def cozmo_program(robot: cozmo.robot.Robot):
     take_picture(robot, 10)
     print('10')
     # Arret 11 (parcours losange)
+    empileCube(robot)
     robot.drive_straight(distance_mm(80), speed_mmps(50)).wait_for_completed()
     robot.turn_in_place(degrees(90)).wait_for_completed()
     take_picture(robot, 11)
@@ -205,10 +292,11 @@ def cozmo_program(robot: cozmo.robot.Robot):
     take_picture(robot, 14)
     print('14')
     # Arret 15 (parcours losange)
+    desempileCube(robot)
     robot.drive_straight(distance_mm(80), speed_mmps(50)).wait_for_completed()
     take_picture(robot, 15)
     print('15')
 
 
 if __name__ == '__main__':
-    cozmo.run_program(cozmo_program)
+    cozmo.run_program(cozmo_program, use_3d_viewer=True, show_viewer_controls=True)
