f199c4d62ede5e1a80b6de53a57f704bb6d57d8d
Cpttran <tran-duy-khuong.ngo.1@ens.etsmtl.ca>
2019-03-14 23:59:02 -0400

    clean up code structure

--- labo2/imgprocess/imageprocessTest.py
+++
@@ -1,8 +0,0 @@
-from imgprocess.imageprocess import process_image
-import cv2
-
-
-# Test code for image processing function
-cropped_img = process_image('../Images/Losanges/Losanges_4_F/8-losange4.jpg')
-cv2.imshow('cropped image', cropped_img)
-cv2.waitKey(0)
