ce57ab2f108f33c236a1683257e3ff8bad8aa5cd
Tran Ngo <ngotran14@gmail.com>
2019-04-03 22:54:44 -0400

    added aima

--- labo3/main.py
+++ labo3/main.py
@@ -1,71 +1,52 @@
-import sys
-import time
-
-import cozmo
-import cozmo.robot
-import asyncio
-from cozmo.objects import CustomObjectMarkers,FixedCustomObject,EvtObjectLocated,CustomObjectTypes, CustomObject,\
-    EvtObjectAppeared,EvtObjectDisappeared
-from cozmo.world import World,CameraImage,EvtNewCameraImage
-from PIL import Image, ImageDraw
-from cozmo.util import degrees, distance_mm, speed_mmps
-from cozmo.audio import AudioEvents
-from cozmo.camera import EvtNewRawCameraImage
-
-import labo3.interactions.tap as tap
-# import labo3.interactions.marker as marker
+import nltk
+from nltk import load_parser
+from logic import *
 
 
-def handle_object_appeared(evt, **kw):
-    # This will be called whenever an EvtObjectAppeared is dispatched -
-    # whenever an Object comes into view.
-    if isinstance(evt.obj, CustomObject):
-        print("Cozmo started seeing a %s" % str(evt.obj.object_type))
-        marker_interaction(evt.obj)
+def example(s1):
+    ep = load_parser('./635/analysesyntaxique.fcfg')
+    for tree in ep.parse(s1.split()):
+        tree.draw()
 
 
-def handle_object_disappeared(evt, **kw):
-    # This will be called whenever an EvtObjectDisappeared is dispatched -
-    # whenever an Object goes out of view.
-    if isinstance(evt.obj, CustomObject):
-        print("Cozmo stopped seeing a %s" % str(evt.obj.object_type))
+sents = [
+    'il faut une corde pour pendre personne',
+    'il faut un couteau pour poignarder Black',
+    'Black est à la cuisine',
+    'Mustard est au salon',
+    'Mustard saigne'
+]
 
 
-def custom_objects(robot: cozmo.robot.Robot):
-    circle2 = robot.world.define_custom_wall(CustomObjectTypes.CustomType00,
-                                             CustomObjectMarkers.Circles2,
-                                             50, 50,
-                                             50, 50, True)
+def printResults(results):
+    for result in results:
+        for (synrep, semrep) in result:
+            # print(synrep)
+            print(semrep)
 
 
-def marker_interaction(robot, object):
-    action = cozmo.robot.Robot.go_to_object(object, distance_mm(70.0))
-    action.wait_for_completed()
-    cozmo.robot.Robot.say_text("What is this?").wait_for_completed()
+noSolutionFound = True
+clauses = []
 
+clauses.append(expr("Loc(x,y) & Suspect(x) & Victime(v) & Access(x,z) & Mortpar(v,z) ==> Solution(x,y,z)"))
+clauses.append(expr("Loc(x,y) & Loc(z,y) ==> Access(x,z)"))
+clauses.append(expr("Saigne(x) & Instr(Couteau) ==> Mortpar(x,Couteau)"))
+clauses.append(expr("Etouffer(x) & Instr(Corde) ==> Mortpar(x,Corde)"))
 
-def cozmo_program(robot: cozmo.robot.Robot):
-    print("Starting Investigation")
-    robot.add_event_handler(robot, cozmo.objects.EvtObjectAppeared, handle_object_appeared)
-    robot.add_event_handler(cozmo.objects.EvtObjectDisappeared, handle_object_disappeared)
+while noSolutionFound:
+    clauses.append(expr("Victime(Mustard)"))
+    clauses.append(expr("Loc(Black, Salon)"))
+    clauses.append(expr("Loc(Couteau, Salon)"))
+    clauses.append(expr("Saigne(Mustard)"))
+    clauses.append(expr("Instr(Couteau)"))
+    clauses.append(expr("Suspect(Black)"))
 
-    cube = None
-    knife = custom_objects(robot)
+    crime_kb = FolKB(clauses)
 
-    look_around = robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
+    answer = fol_fc_ask(crime_kb, expr('Solution(x,y,z)'))
 
-    try:
-        cube = robot.world.wait_for_observed_light_cube(timeout=60)
-    except asyncio.TimeoutError:
-        print("Didn't find a cube :-(")
-        return
-    finally:
-        look_around.stop()
+print('-------------------------------------------------------------------------------------')
+printResults(nltk.interpret_sents(sents, './635/analysesyntaxique.fcfg'))
 
-    if cube:
-        interaction = tap.get_interaction(cube)
-        print(interaction)
-
-
-if __name__ == '__main__':
-    cozmo.run_program(cozmo_program, use_3d_viewer=True, show_viewer_controls=True, use_viewer=True)
+# example('Mustard est au salon')
+print('end of program')
